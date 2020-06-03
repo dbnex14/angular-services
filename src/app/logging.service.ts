af79d1eac4c5365a6ef1ea3a828df21293560252
Buljubasic <KI0V@icbc.net>
2020-06-03 08:59:55 -0700

    minor comment change

--- src/app/logging.service.ts
+++ src/app/logging.service.ts
@@ -1,5 +1,7 @@
 
-// service is just normal typescript class
+// service is just normal typescript class.  we use services to avoid code
+// duplication so here we centralize functionality we can then use all over
+// our app.
 export class LoggingService {
     logStatusChange(status: string) {
         console.log('A server status changed, new status: ' + status);
